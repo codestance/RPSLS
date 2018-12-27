028cec219a7ca8b85187be30d1a297b18a1b44a6
Unknown <skrzynska.konstancja@gmail.com>
2018-12-27 14:50:15 +0100

    final touches

--- js.js
+++ js.js
@@ -10,15 +10,11 @@ const spockDiv = document.getElementById('spock');
 const userChoiceDiv = document.querySelector('#user-choice');
 const compChoiceDiv = document.querySelector('#comp-choice');
 const resultDiv = document.querySelector('.result');
-
-
-
 function getCompChoice(){
     const weapons = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
     const weapon = weapons[Math.floor(Math.random()*weapons.length)];
     return weapon;
 };
-
 const isItWin = {
     rock : {
         lizard: 'Rock crushes Lizard',
@@ -41,12 +37,11 @@ const isItWin = {
         rock: 'Spock vaporizes Rock'
     }
 }
-
 function game(userChoice){
-    // userChoiceDiv.appendChildImg(userChoice);
+    appendChildImg(userChoiceDiv, userChoice);
     console.log('userChoice : ' + userChoice);
     const compChoice = getCompChoice();
-    // compChoiceDiv.appendChildImg(compChoice);
+    appendChildImg(compChoiceDiv, compChoice);
     console.log('compChoice : '+ compChoice);
     var msg;
     if(userChoice===compChoice){
@@ -72,34 +67,49 @@ function game(userChoice){
     if(resultDiv.firstChild){
         resultDiv.removeChild(resultDiv.firstChild);
     }
-    resultDiv.appendChild(msg);
-    
+    resultDiv.appendChild(msg);    
 }
-
-// function appendChildImg(e){
-
-// }
-
+function appendChildImg(d, i){
+    let img = document.createElement('img');
+    var src;
+    switch(i){
+        case 'rock':
+            src = 'img/rock.png';
+            break;
+        case 'paper':
+            src = 'img/paper.png';
+            break;
+        case 'scissors':
+            src = 'img/scissors.png';
+            break;
+        case 'lizard':
+            src = 'img/lizard.png';
+            break;
+        case 'spock':
+            src = 'img/spock.png';
+            break;
+    }
+    if(d.firstChild){
+        d.removeChild(d.firstChild);
+    }
+    img.setAttribute('src', src);
+    d.appendChild(img);
+}
 function main(){
     rockDiv.addEventListener('click', function(){
         game('rock');
     })
-    
     paperDiv.addEventListener('click', function(){
         game('paper');
-    })
-    
+    })   
     scissorsDiv.addEventListener('click', function(){
         game('scissors');
     })
-    
     lizardDiv.addEventListener('click', function(){
         game('lizard');
     })
-    
     spockDiv.addEventListener('click', function(){
         game('spock');
     })
 };
-
 main();
